2a661d76df3fc85933e23c8bc024111fafe6709b
Chedli <chedlidon@gmail.com>
2019-05-16 14:26:16 +0200

    comments

--- single.php
+++ single.php
@@ -8,15 +8,28 @@
                   
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
             get_template_part( 'content-single', get_post_format() );
-            if ( comments_open() || get_comments_number() ) : comments_template();
-            endif; endwhile; endif; ?>
+             // If comments are open or we have at least one comment, load up the comment template.
+            if ( comments_open() || get_comments_number() ) : ?>
+                <div class="pt-5">
+              <h3 class="mb-5"><?php echo get_comments_number().'  '; ?> Comments</h3>
+            
+                    <p>
+               <?php comments_template(); ?>
+                        </p>
+                   
+                  </div>
+               
+                        <?php
+            endif;  
+            endwhile; endif; ?>
                     
 </div>
-                
+               
 </div>
-              <?php get_sidebar(); ?>
+            <?php get_sidebar(); ?>   
 </div>
 </div>
+
 
     
       </section>
